73485ccddf2e1d3fb2f168b70eabc4419573f9b2
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-08 08:55:44 -0300

    Fixes connection adapter options typo

--- lib/active_record/connection_adapter.rb
+++ lib/active_record/connection_adapter.rb
@@ -4,7 +4,7 @@ module ActiveRecord
     class SqliteAdapter
       def initialize(file)
         require 'sqlite3'
-        @db = SQLite3::Database.new(file.to_s, result_as_hash: true)
+        @db = SQLite3::Database.new(file.to_s, results_as_hash: true)
       end
 
       # Execute an SQL query and return the results as an array of hashes.
