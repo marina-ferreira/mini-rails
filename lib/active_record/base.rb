03e4765cdb3c1d989e18c579bc0d9ae17c42a454
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-08 15:28:26 -0300

    Extracts base to file and autoloads in in root

---
+++ lib/active_record/base.rb
@@ -0,0 +1,47 @@
+module ActiveRecord
+  class Base
+    def initialize(attributes = {})
+      @attributes = attributes
+    end
+
+    def self.abstract_class=(value)
+      # Not implemented
+    end
+
+    def self.table_name
+      name.downcase + 's'
+    end
+
+    def self.find(id)
+      find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i}").first
+    end
+
+    def self.all
+      find_by_sql("SELECT * FROM #{table_name}")
+    end
+
+    def self.find_by_sql(sql)
+      connection.execute(sql).map { |attributes| new(attributes) }
+    end
+
+    def self.establish_connection(options)
+      # @@var is a class variable, shared across all instances
+      @@connection = ConnectionAdapter::SqliteAdapter.new(options[:database])
+    end
+
+    def self.connection
+      @@connection
+    end
+
+    # Returns records attribute value
+    def method_missing(name, *args)
+      columns = self.class.connection.columns(self.class.table_name)
+
+      if columns.include?(name)
+        @attributes[name]
+      else
+        super
+      end
+    end
+  end
+end
