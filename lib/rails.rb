c9bd836fcc8eba37fd378109c6a3e700f121953d
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-13 09:00:55 -0300

    Railties | Rails module

---
+++ lib/rails.rb
@@ -0,0 +1,15 @@
+module Rails
+  autoload :Application, 'rails/application'
+
+  def self.env
+    ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+  end
+
+  def self.groups
+    [:default, env]
+  end
+
+  def self.application
+    Application.instance
+  end
+end
