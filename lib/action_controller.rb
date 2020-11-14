2bb7556f4d8df3316245fb9f8323c855a05eb297
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-14 19:48:13 -0300

    Action Controller | Autoloads RequestForgeryProtection and includes it in Base

--- lib/action_controller.rb
+++ lib/action_controller.rb
@@ -2,4 +2,5 @@ module ActionController
   autoload :Base, 'action_controller/base'
   autoload :Callbacks, 'action_controller/callbacks'
   autoload :Metal, 'action_controller/metal'
+  autoload :RequestForgeryProtection, 'action_controller/request_forgery_protection'
 end
--- lib/action_controller/base.rb
+++ lib/action_controller/base.rb
@@ -1,5 +1,6 @@
 module ActionController
   class Base < Metal
     include Callbacks
+    include RequestForgeryProtection
   end
 end
