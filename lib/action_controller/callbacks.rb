a5d75d853f02f985e101510e5bfef0b23aed7c27
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-14 13:13:27 -0300

    Action Controller | Pattern to include class level methods on a module

--- lib/action_controller/callbacks.rb
+++ lib/action_controller/callbacks.rb
@@ -1,5 +1,17 @@
 module ActionController
-  class Callbacks
+  module Callbacks
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
+    module ClassMethods
+      def before_action(method, options = {})
+        before_actions << Callback.new(method, options)
+      end
+
+      def before_actions
+        @before_action ||= []
+      end
+    end
   end
 end
