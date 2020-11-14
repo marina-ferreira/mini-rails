38682bb26605a7173551db3e34240ee568ced707
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-14 13:14:10 -0300

    Action Controller | Callbacks implements before_action

--- lib/action_controller/callbacks.rb
+++ lib/action_controller/callbacks.rb
@@ -1,7 +1,36 @@
 module ActionController
   module Callbacks
+    class Callback
+      def initialize(method, options)
+        @method = method
+        @options = options
+      end
+
+      def match?(action)
+        if @options[:only]
+          @options[:only].include? action.to_sym
+        else
+          true
+        end
+      end
+
+      def call(controller)
+        controller.send(@method)
+      end
+    end
+
     def self.included(base)
       base.extend ClassMethods
+    end
+
+    def process(action)
+      self.class.before_actions.each do |callback|
+        if callback.match?(action)
+          callback.call(self)
+        end
+      end
+
+      super
     end
 
     module ClassMethods
