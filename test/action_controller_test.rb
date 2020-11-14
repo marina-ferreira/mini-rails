8234633c52b993a6e50c473db7ee0c7c14d66800
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-14 13:15:10 -0300

    Action Controller | Callbacks test

--- test/action_controller_test.rb
+++ test/action_controller_test.rb
@@ -2,8 +2,20 @@ require 'test_helper'
 
 class ActionControllerTest < MiniTest::Test
   class TestController < ActionController::Base
+    before_action :callback, only: [:show]
+
     def index
       response << 'index'
+    end
+
+    def show
+      response << 'show'
+    end
+
+    private
+
+    def callback
+      response << 'callback'
     end
   end
 
@@ -13,5 +25,13 @@ class ActionControllerTest < MiniTest::Test
     controller.process :index
 
     assert_equal ['index'], controller.response
+  end
+
+  def test_callbacks
+    controller = TestController.new
+    controller.response = []
+    controller.process :show
+
+    assert_equal ['callback', 'show'], controller.response
   end
 end
