d9c9cfd078972aabc1c5418f4a1e2348b17a6368
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-14 19:48:52 -0300

    Action Controller | Core Extensions Hash

--- lib/active_support.rb
+++ lib/active_support.rb
@@ -1,4 +1,5 @@
 require 'active_support/core_ext/string' # has to be required because it has already been loaded
+require 'active_support/core_ext/hash'
 
 module ActiveSupport
   autoload :Dependencies, 'active_support/dependencies'
