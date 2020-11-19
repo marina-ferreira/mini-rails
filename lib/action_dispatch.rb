28635fc967b820782e13bae0a58aac1814821287
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-19 08:51:48 -0300

    Action Dispatch | autoload routing module

--- lib/action_dispatch.rb
+++ lib/action_dispatch.rb
@@ -1,4 +1,6 @@
 module ActionDispatch
-  autoload :Routing, 'action_dispatch/routing'
-  autoload :Mapper, 'action_dispatch/mapper'
+  module Routing
+    autoload :RouteSet, 'action_dispatch/routing/route_set'
+    autoload :Mapper, 'action_dispatch/routing/mapper'
+  end
 end
