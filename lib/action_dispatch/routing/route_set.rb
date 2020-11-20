0876f77e1468b348bfa09c8bc1d046d32c96657f
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-20 09:04:03 -0300

    Action Dispatch | Route set call method

--- lib/action_dispatch/routing/route_set.rb
+++ lib/action_dispatch/routing/route_set.rb
@@ -5,6 +5,19 @@ module ActionDispatch
       def match?(request)
         request.request_method == method && request.path_info == path
       end
+
+      def controller_class
+        "#{controller.classify}Controller".constantize
+      end
+
+      def dispatch(request)
+        controller = controller_class.new
+        controller.request = request
+        controller.response = Rack::Response.new
+
+        controller.process(action)
+        controller.response.finish
+      end
     end
 
     class RouteSet
@@ -25,6 +38,16 @@ module ActionDispatch
       def draw(&block)
         mapper = Mapper.new(self)
         mapper.instance_eval(&block)
+      end
+
+      def call(env)
+        request = Rack::Request.new(env)
+
+        if route = find_route(request)
+          route.dispatch(request)
+        else
+          [404, { 'Context-Type' => 'text/plain' }, ['Not found']]
+        end
       end
     end
   end
