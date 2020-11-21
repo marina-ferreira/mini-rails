46ca54161929d35b90620d8672366c604108986c
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-21 20:59:45 -0300

    Action View | Template render test

--- test/action_view_test.rb
+++ test/action_view_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class ActionViewTest < MiniTest::Test
   def test_render_template
+    template = ActionView::Template.new('<p>Hello<p>', 'test_helper_template')
+    context = ActionView::Base.new
 
+    assert_equal '<p>Hello<p>', template.render(context)
   end
 end
