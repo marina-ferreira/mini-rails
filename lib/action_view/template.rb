7aa2dc11e019805080dad0128bd83e6098adde01
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-21 20:59:35 -0300

    Action View | Template render implementation

--- lib/action_view/template.rb
+++ lib/action_view/template.rb
@@ -3,6 +3,29 @@ module ActionView
     def initialize(source, name)
       @source = source
       @name = name
+      @compiled = false
+    end
+
+    def render(context)
+      compile
+      context.send(method_name)
+    end
+
+    def method_name
+      @name.gsub(/[^\w]/, '_')
+    end
+
+    def compile
+      return if @compiled
+      code = ERB.new(@source).src
+
+      CompiledTemplates.module_eval <<-CODE
+        def #{method_name}
+          #{code}
+        end
+      CODE
+
+      @compiled = true
     end
   end
 end
