15e530a744f9055e5013f136914b2459ad8726ad
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-21 18:11:01 -0300

    Action Dispatch | Adds default middleware stack to rails application

--- lib/rails/application.rb
+++ lib/rails/application.rb
@@ -1,3 +1,5 @@
+require 'sprockets'
+
 module Rails
   class Application
     def self.inherited(klass)
@@ -25,6 +27,36 @@ module Rails
 
     def routes
       @routes ||= ActionDispatch::Routing::RouteSet.new
+    end
+
+    def default_middleware_stack
+      Rack::Builder.new do
+        use Rack::ContentLength
+        use Rack::CommonLogger
+        use Rack::ShowExceptions
+
+        use Rack::Static, urls: ['/favicon.ico', '/robots.txt'],
+                          root: Rails.root.join('public')
+
+        map '/assets' do
+          sprockets = Sprockets::Environment.new
+          sprockets.append_path Rails.root.join('app/assets/javascript')
+          sprockets.append_path Rails.root.join('app/assets/stylesheets')
+          run sprockets
+        end
+      end
+    end
+
+    def app
+      @app ||= begin
+        stack = default_middleware_stack
+        stack.run routes
+        stack.to_app
+      end
+    end
+
+    def call(env)
+      app.call(env)
     end
   end
 end
