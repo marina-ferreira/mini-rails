a00f8d4f61054347a8cf1e6df5fbf4a92309843d
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-11-22 08:46:21 -0300

    Action View | Posts controller index endpoint

--- test/muffin_blog/app/controllers/posts_controller.rb
+++ test/muffin_blog/app/controllers/posts_controller.rb
@@ -3,11 +3,6 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-
-    response.write "<h1>The Muffin Blog</h1>"
-    @posts.each do |post|
-      response.write "<h2>#{post.title}</h2>"
-    end
   end
 
   def show
